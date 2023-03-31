6351c077e295fe7f29fd8cde98dd2ca2bf3c54c6
Itec ICT Hub <59688669+Itecrom@users.noreply.github.com>
2023-03-31 21:01:02 +0200

    Python-network redo

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE $1 -L
+# Script sends a DELETE request to URL passed as the first argument
+curl -sX "DELETE" "$1"
